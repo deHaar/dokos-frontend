9aa528f24baeebb8ccf991afbdda41a5041e399b
Matthias Herrmann <matthias.herrmann@lgln.niedersachsen.de>
2025-05-13 17:41:46 +0200

    Add custom component, custom icon and change some text

--- src/App.vue
+++ src/App.vue
@@ -1,19 +1,21 @@
 <script setup lang="ts">
 import HelloWorld from './components/HelloWorld.vue'
 import TheWelcome from './components/TheWelcome.vue'
+import PlayerTable from './components/PlayerTable.vue'
 </script>
 
 <template>
   <header>
-    <img alt="Vue logo" class="logo" src="./assets/logo.svg" width="125" height="125" />
+    <img alt="Vue logo" class="logo" src="./assets/four-aces.svg" width="125" height="125" />
 
     <div class="wrapper">
-      <HelloWorld msg="You did it!" />
+      <HelloWorld msg="DokOS" />
     </div>
   </header>
 
   <main>
-    <TheWelcome />
+    <!-- <TheWelcome /> -->
+     <PlayerTable />
   </main>
 </template>
 
---
+++ src/components/PlayerTable.vue
@@ -0,0 +1,70 @@
+<script setup lang="ts">
+
+</script>
+
+<template>
+    <div>
+        <h1>Spieler-Tabelle</h1>
+    </div>
+    <div>
+        <table class="round-table">
+            <tr>
+                <th>Player 1</th>
+                <th>Player 2</th>
+                <th>Player 3</th>
+                <th>Player 4</th>
+                <th colspan="2">Game</th>
+            </tr>
+            <tr>
+                <th>12</th>
+                <th>6</th>
+                <th>-10</th>
+                <th>-8</th>
+                <th>Value</th>
+                <th>Bock</th>
+            </tr>
+            <tr>
+                <td>2</td>
+                <td>2</td>
+                <td>-2</td>
+                <td>-2</td>
+                <td>2</td>
+                <td>0</td>
+            </tr>
+            <tr>
+                <td>6</td>
+                <td>-6</td>
+                <td>-6</td>
+                <td>-2</td>
+                <td>2</td>
+                <td>0</td>
+            </tr>
+            <tr>
+                <td>4</td>
+                <td>-4</td>
+                <td>-4</td>
+                <td>-2</td>
+                <td>2</td>
+                <td>0</td>
+            </tr>
+        </table>
+    </div>
+</template>
+
+<style scoped>
+.round-table td {
+  text-align: center;
+}
+
+.round-table th {
+    font-weight: bold;
+}
+
+.positive-value th, td {
+    color: green;
+}
+
+.negative-value th, td {
+    color: red;
+}
+</style>
